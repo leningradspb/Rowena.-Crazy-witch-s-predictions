5622581c1d1af32067a162d27a4c2e1517be7d1f
Ed <sinyakov.gsg@gmail.com>
2019-05-15 13:40:35 +0300

    Initial Commit

---
+++ Rowena. Crazy witch's predictions/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Rowena. Crazy witch's predictions
+//
+//  Created by Eduard Sinyakov on 15/05/2019.
+//  Copyright © 2019 Eduard Sinyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
